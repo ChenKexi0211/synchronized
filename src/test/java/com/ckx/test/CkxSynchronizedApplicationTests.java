61543ebd9eb4d777f1c61b08cf43daf519049178
chenkexi <kexi.chen@androidmov.com>
2019-09-09 16:11:09 +0800

    创建项目

---
+++ src/test/java/com/ckx/test/CkxSynchronizedApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ckx.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CkxSynchronizedApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
